4aea9fbff6fbca2a996e3d1ed918ca6ba84fbdb2
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-25 17:26:12 +0530

    work on Home Page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,23 @@
-import "./Home.css"
+import "./Home.css";
+import homeBowl from "/Images/homeBowl.png"
+import mainText from "/Images/mainText.png"
 
 function Home() {
   return (
     <div>
       <div className="homeContainer">
-        hl
+        <div className="homeTextContainer">
+          <div className="homeTextManager">
+            <h1>Wholesome Recipes <br /> for Every Occasion</h1>
+            <p>Goodness In Bowl, to Warm Your Soul</p>
+            <img src={mainText} alt="" />
+          </div>
+        </div>
+        <div className="homeImageContainer">
+          <div className="homeBowlContainer">
+            <img src={homeBowl} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
