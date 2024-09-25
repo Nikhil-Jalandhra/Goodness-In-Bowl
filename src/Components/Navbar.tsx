79a6b0bb1b4c6373d89c64dea22ddc36f47da5c8
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-25 10:31:52 +0530

    some basic components

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,38 @@
+import "./NavBar.css"
+import { GiBowlOfRice } from "react-icons/gi";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    const navLink = [
+        { name: 'Home', link: '/', key: "Home" },
+        { name: 'About', link: '/about', key: "About Us" },
+        { name: 'Blog', link: '/blog', key: "Blog" },
+        { name: 'Order', link: '/Order', key: "Order Now" },
+    ]
+
+  return (
+    <div>
+      <div className="navContainer">
+        <div className="navLogoContainer">
+            <div className="logoContainer">
+                <h1>GIB</h1>
+                <span><GiBowlOfRice /></span>
+            </div>
+        </div>
+        <div className="navLinkContainer">
+            <div className="navLinkBox">
+                {navLink.map((item) => (
+                    <Link to={item.link} key={item.key}>
+                        <li className="navLinkList">{item.name}</li>
+                    </Link>
+                ))}
+            </div>
+        </div>
+      </div>
+      <div className="navSeperateLine"></div>
+    </div>
+  );
+}
+
+export default Navbar;
