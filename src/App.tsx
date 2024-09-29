da72c41f6240c1e36d1a1784620e4de7aa07d1e5
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-29 12:11:36 +0530

    add more pages in website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Blog from './Pages/Blog';
+import Recipes from './Pages/Recipes';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BlogSummary from './Pages/BlogSummary';
+import RecipeSummary from './Pages/RecipeSummary';
 
 function App() {
 
@@ -16,6 +19,9 @@ function App() {
         <Route element={<Home/>} path='/'/>
         <Route element={<About/>} path='/about'/>
         <Route element={<Blog/>} path='/blog'/>
+        <Route element={<BlogSummary/>} path='/blogSummary/:id/:title'/>
+        <Route element={<Recipes/>} path='/Recipes'/>
+        <Route element={<RecipeSummary/>} path='/recipeSummary/:id/:title'/>
       </Routes>
       <Footer/>
     </Router>
